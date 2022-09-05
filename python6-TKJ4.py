b3ee1a20baa90345fda7a1571cb373703c1c4e2f
dew <112526224+cry4bb@users.noreply.github.com>
2022-09-05 09:45:39 +0700

    Add files via upload

---
+++ python6-TKJ4.py
@@ -0,0 +1,12 @@
+list_kosong = []
+list_buah = ["pisang","nanas", "melon", "durian"]
+list_nilai = [80, 70, 90, 60]
+list_jawaban = [150, 33.33, 'Presiden Sukarno']
+
+
+print('list_kosong:', list_kosong)
+print('list_buah:', list_buah)
+print('list_nilai:', list_nilai)
+print('list_jawaban:', list_jawaban)
+
+
