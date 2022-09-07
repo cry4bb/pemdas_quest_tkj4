928e91a59e39f0128cb1db17986577e832866f99
dew <112526224+cry4bb@users.noreply.github.com>
2022-09-07 08:46:47 +0700

    Add files via upload

---
+++ python7-tkj4.py
@@ -0,0 +1,27 @@
+list_kosong = []
+list_integer = [1, 2, 3, 10, 100, 250]
+list_string = ["Dewi", "nazla", "lulu"]
+list_mix = [20 "Arya", True, "Bryan"]
+
+print("list kosong", list_kosong)
+print("list data integer", list_integer)
+print("list data string", list_string)
+print("list data mix", list_mix)
+
+print("Data Kedua List String:", list_string[1])
+
+print("Data Pertama List Integer:", list_integer[0])
+print("Data Kedua List Integer:", list_integer[1])
+print("Data Ketiga List Integer:", list_integer[2])
+print("Data Keempat List Integer:",list_integer[3] )
+print("Data Kelima List Integer:",) list_integer[4]
+print("Data Keenam List Integer:", list_integer[5])
+
+print("Data Pertama List String:", list_string[0])
+print("Data Kedua List String:", list_string[1])
+print("Data Ketiga List String:", list_string[2])
+
+print("Data Pertama List Data Mix:", list_mix[0])
+print("Data Kedua List Data Mix:", list_mix[1])
+print("Data Ketiga List Data Mix:", list_mix[2])
+print("Data Keempat List Data Mix:", list_mix[3])
