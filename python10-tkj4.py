d0824736b9bb8f416b7aaa26708b0f14d7631f1f
dew <112526224+cry4bb@users.noreply.github.com>
2022-10-10 09:46:33 +0700

    Add files via upload

---
+++ python10-tkj4.py
@@ -0,0 +1,27 @@
+# Membuat tipe data koleksi
+
+koleksi_data_str = ["Pisang" , "Mangga" , "Jambu" , "Semangka" ]
+
+koleksi_data_int = [200, 300, 400, 500]
+
+koleksi_data_mix = ["Rizky Billar" , 100 , True , "Reza Arap"]
+
+print("koleksi data string:", koleksi_data_str)
+
+print("koleksi data integear:", koleksi_data_int)
+
+print("koleksi data campuran:", koleksi_data_mix)
+
+# Buatlah 3 kumpulan data: nama hewan, nilai uts, nama teman sebangku kalian
+
+koleksi_nama_hewan = ["sapi" , "kambing" , "domba"]
+
+koleksi_nilai_uts = [90 , 80 , 86 ]
+
+koleksi_nama_teman = ["luul mukaromah", 100 , True , "nazla levianna"]
+
+print("koleksi data string", koleksi_nama_hewan)
+
+print("koleksi data integear", koleksi_nilai_uts)
+
+print("koleksi data campuran", koleksi_nama_teman)
