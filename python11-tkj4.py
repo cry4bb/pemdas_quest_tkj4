6826efb08674d64ba31b531aa2d03a0a84a3af26
dew <112526224+cry4bb@users.noreply.github.com>
2022-10-18 09:45:08 +0700

    Add files via upload

---
+++ python11-tkj4.py
@@ -0,0 +1,19 @@
+#general
+gender =("laki-laki", "perempuan")
+
+#menggunakan fungsi tuple, tuple ()
+tuple_name = tuple(["jayabaya", "leborn james"])
+
+#tuple tunggal
+tuple_tunggal = ("kazu",)
+
+gabungan = gender + tuple_name 
+print(gabungan)
+
+makanan_daerah = ("ketoprak", "pempek", "rendang", "papeda")
+hewan_mamalia = ("tupai", "hamster", "kucing", "sapi")
+hewan_ovipar = ("ayam", "bebek", "burung", "ular")
+
+print(makanan_daerah)
+print(hewan_mamalia)
+print(hewan_ovipar)
